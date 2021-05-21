0437642236eeb148023785c848f81329822d4ac7
Filimon Raluca <raluca.filimon@info.uaic.ro>
2021-05-21 14:54:53 +0300

    Accounts

---
+++ Accounts/view/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Login from './components/login'
+import SignIn from "./components/signin"
+
+function App() {
+  return (
+    <div className="App">
+      <Login></Login>
+      <SignIn></SignIn>
+    </div>
+  );
+}
+
+export default App;
