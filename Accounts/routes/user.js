f98d5b510e5e961c8dae814192d88f6f03a9a920
Andra Simion <andrasimion99@gmail.com>
2021-05-30 12:14:11 +0300

    update autentificat

--- Accounts/routes/user.js
+++ Accounts/routes/user.js
@@ -1,65 +1,68 @@
-const express = require('express')
-const router = express.Router()
-const User = require('../model/User')
-const verifyToken = require('./verifyToken')
-const { registerUser, loginUser, getUsers, getUserById,changeUserInfo, deleteUser, loginGoogleUser } = require('../controller/user')
+const express = require("express");
+const router = express.Router();
+const User = require("../model/User");
+const verifyToken = require("./verifyToken");
+const {
+  registerUser,
+  loginUser,
+  getUsers,
+  getUserById,
+  changeUserInfo,
+  deleteUser,
+  loginGoogleUser,
+} = require("../controller/user");
 
 const statusCodes = require("../config/configurations").statusCodes;
 
-
-router.post('/', async (req, res) => {
-  const result = await registerUser(req, res)
+router.post("/", async (req, res) => {
+  const result = await registerUser(req, res);
   res.json(result);
 });
 
-router.post('/login/google', async (req, res) => {
-  const result = await loginGoogleUser(req, res)
-  const statusCode = result.success
-    ? statusCodes.OK
-    : statusCodes.BAD_REQUEST;
+router.post("/login/google", async (req, res) => {
+  const result = await loginGoogleUser(req, res);
+  const statusCode = result.success ? statusCodes.OK : statusCodes.BAD_REQUEST;
   res.status(statusCode).json(result);
 });
 
-router.post('/login', async (req, res) => {
-  const result = await loginUser(req, res)
-  const statusCode = result.success
-    ? statusCodes.OK
-    : statusCodes.BAD_REQUEST;
+router.post("/login", async (req, res) => {
+  const result = await loginUser(req, res);
+  const statusCode = result.success ? statusCodes.OK : statusCodes.BAD_REQUEST;
   res.status(statusCode).json(result);
 });
 
-router.get('/test',verifyToken, (req, res) => {
+router.get("/test", verifyToken, (req, res) => {
   res.send("OK");
-})
+});
 
-router.get('/:userId', async (req, res) => {
-  const result = await getUserById(req, res)
+router.get("/:userId", async (req, res) => {
+  const result = await getUserById(req, res);
   res.json(result);
-})
+});
 
-router.get('/', async (req, res) => {
-  const result = await getUsers(req, res)
+router.get("/", async (req, res) => {
+  const result = await getUsers(req, res);
   res.json(result);
-})
+});
 
-router.put('/', (req,res) => {
+router.put("/", (req, res) => {
   res.status(statusCodes.NOT_ALLOWED);
-  res.json({message: "Method Not Allowed"})
-})
+  res.json({ message: "Method Not Allowed" });
+});
 
-router.put('/:userId', async (req, res) => {
-  const result = await changeUserInfo(req, res)
+router.put("/:userId", verifyToken, async (req, res) => {
+  const result = await changeUserInfo(req, res);
   res.json(result);
-})
+});
 
-router.delete('/', (req,res) => {
+router.delete("/", (req, res) => {
   res.status(statusCodes.NOT_ALLOWED);
-  res.json({message: "Method Not Allowed"})
-})
+  res.json({ message: "Method Not Allowed" });
+});
 
-router.delete('/:userId', async (req, res) => {
-  const result = await deleteUser(req, res)
+router.delete("/:userId", async (req, res) => {
+  const result = await deleteUser(req, res);
   res.json(result);
-})
+});
 
 module.exports = router;
--- Accounts/routes/verifyToken.js
+++ Accounts/routes/verifyToken.js
@@ -1,24 +1,24 @@
-const { response } = require('express');
-const dotenv = require('dotenv')
-const jwt = require('jsonwebtoken');
+const { response } = require("express");
+const dotenv = require("dotenv");
+const jwt = require("jsonwebtoken");
 const statusCodes = require("../config/configurations").statusCodes;
 
-dotenv.config({ path: './config/config.env' })
+dotenv.config({ path: "./config/config.env" });
 const verifyToken = (req, res, next) => {
-    console.log("HERE")
-    if (typeof req.headers.authorization !== 'string') {
-        res.sendStatus(statusCodes.BAD_REQUEST);
-        return;
-      }
-    try{
-        console.log(req.headers.authorization)
-        const token = req.headers.authorization.split(" ")
-        if (token[0] === 'Bearer' && jwt.verify(token[1],process.env.TOKEN_SECRET))
-            next()
-    }catch(e){
-            res.status(statusCodes.UNAUTHORIZED)
-            res.json({ success: false, data: { e } })
-    }
-}
+  if (typeof req.headers.authorization !== "string") {
+    res.status(statusCodes.UNAUTHORIZED);
+    res.json({ success: false, data: { message: "Access Denied!" } });
+    return;
+  }
+  try {
+    console.log(req.headers.authorization);
+    const token = req.headers.authorization.split(" ");
+    if (token[0] === "Bearer" && jwt.verify(token[1], process.env.TOKEN_SECRET))
+      next();
+  } catch (e) {
+    res.status(statusCodes.UNAUTHORIZED);
+    res.json({ success: false, data: { e } });
+  }
+};
 
 module.exports = verifyToken;
